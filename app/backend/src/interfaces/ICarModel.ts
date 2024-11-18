4245907f2b21f4487716f8c118b9bb833763de69
Douglas Araújo <araujodgdev@gmail.com>
2024-11-18 10:54:25 -0300

    feat: BACKEND={Cria o model de Carro para controlar as operações no banco de dados com um singleton do prisma client

---
+++ app/backend/src/interfaces/ICarModel.ts
@@ -0,0 +1,8 @@
+import { Car } from "@prisma/client";
+
+export type InsertCar = Omit<Car, "id">;
+export type SelectCar = Car;
+
+export default interface ICarModel {
+    registerCar(car: InsertCar): Promise<SelectCar | any>;
+}
--- app/backend/src/models/CarModel.ts
+++ app/backend/src/models/CarModel.ts
@@ -1,4 +1,22 @@
+import ICarModel, { InsertCar, SelectCar } from "../interfaces/ICarModel.js";
+import { PrismaClientSingleton } from "../utils/PrismaClient.js";
 
-class CarModel {
-    
+class CarModel implements ICarModel {
+
+    constructor() { }
+
+    async registerCar(car: InsertCar): Promise<SelectCar | null> {
+        try {
+            let newCar = await PrismaClientSingleton.getInstance().car.create({
+                data: car
+            })
+            return newCar;
+        } catch (error) {
+            return null;
+        }
+
+    }
+
 }
+
+export { CarModel };
