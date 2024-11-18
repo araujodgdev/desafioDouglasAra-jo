e6b70ad7b44792e0f19996c6c75f853febeb017e
Douglas Araújo <araujodgdev@gmail.com>
2024-11-18 12:34:49 -0300

    feat: BACKEND={Implementa o controller para deletar um carro}

--- app/backend/src/controllers/CarController.ts
+++ app/backend/src/controllers/CarController.ts
@@ -27,6 +27,15 @@ class CarController implements ICarController {
             return res.status(500).json({ message: error.message });
         }
     }
+
+    public async removeCar(req: Request, res: Response): Promise<Response> {
+        try {
+            const { data, status } = await this.carService.removeCar(req.params.id);
+            return res.status(mapStatusHTTP(status)).json(data);
+        } catch (error: any) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
 }
 
 export { CarController };
