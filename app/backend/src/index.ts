6bb39dc6c2ee89ad290f4d1689da9b85a6fdf72f
Douglas Araújo <araujodgdev@gmail.com>
2024-11-18 10:08:51 -0300

    feat: BACKEND={Configura o banco de dados SQL com docker}

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -3,7 +3,9 @@ import { App } from "./app.js";
 const app = new App();
 
 app.app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send({
+        message: 'Hello World!'
+    });
 });
 
 app.start(Number(process.env.PORT) || 3000);
